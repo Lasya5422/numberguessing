afa6e9fb794d7cca03753d6d6583b5922811bb4b
Lasya5422 <162294266+Lasya5422@users.noreply.github.com>
2024-03-20 19:09:02 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+
+
+def guess_number():
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    print("Welcome to the Number Guessing Game!")
+    print("I have chosen a number between 1 and 100. Try to guess it.")
+
+    while True:
+        guess = int(input("Enter your guess: "))
+        attempts += 1
+
+        if guess < secret_number:
+            print("Too low. Try again.")
+        elif guess > secret_number:
+            print("Too high. Try again.")
+        else:
+            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            break
+
+
+if __name__ == "_main_":
+    guess_number()
